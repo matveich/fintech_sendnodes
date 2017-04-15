51c30b59342a9b742a4dbc760b3e03b7a948a702
Konstantin <rybkin-k@mail.ru>
2017-04-15 11:24:52 +0300

    hui

--- ml.py
+++ ml.py
@@ -5,19 +5,33 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 import pandas as pd
-
-log = open('log.txt', 'w')
-
-train_data = pd.read_csv('...')
+import numpy as np
 
 
 def get_response(message):
+    pass
+
+
+def normalize(data):
+    pass
+
+
+def train(X, y):
     text_clf = Pipeline([
         ('vect', CountVectorizer()),
         ('tfidf', TfidfTransformer()),
         ('clf', MultinomialNB())])
-    text_clf = text_clf.fit()
+    print('Training...')
+    text_clf = text_clf.fit(X, y)
+    return text_clf
 
 
 if __name__ == '__main__':
-    print('lol')
+    log = open('log.txt', 'w')
+    print('Reading...')
+    themes_data = pd.read_csv('themes.csv')
+    train_data = pd.read_csv('train.csv')
+    print('Setting...')
+    text_clf = train(train_data['Speech'], train_data['ThemeLabel'])
+    # print(np.mean(predicted == train_data['ThemeLabel']))
+    print('Done!')
