23d550f2733b94f4f64bf258c77b416adf095f32
Yaroslav <kharchenko.yar@gmail.com>
2017-04-16 13:24:41 +0300

    Исус пожалусто нет

--- server_bot.py
+++ server_bot.py
@@ -9,6 +9,8 @@ from threading import Timer
 bot = telebot.TeleBot(cfg.token)
 model = Model()
 
+users = {}
+
 env_var = {
     'last_theme': None,
     'get_response': None,
@@ -55,7 +57,7 @@ def classify_answer(mtext):
     return 0
 
 
-def check_confirmation(conf_res, expected, message):
+def check_confirmation(conf_res, expected, user_id):
     if conf_res == 1:
         if expected == 'confirmation':
             text = "В ближайшее время на ваш вопрос ответит оператор"
@@ -63,55 +65,55 @@ def check_confirmation(conf_res, expected, message):
             return 'Я жду вопроса ^_^'
         else:
             text = "Error"
-        text = check_currency(text, env_var['last_theme'])
-        env_var['expected'] = 'query'
-        env_var['timer'].cancel()
-        print(env_var['timer_desc'] + "  отменён")
+        text = check_currency(text, users[user_id]['last_theme'])
+        users[user_id]['expected'] = 'query'
+        users[user_id]['timer'].cancel()
+        print(users[user_id]['timer_desc'] + "  отменён")
     else:
         if expected == 'confirmation':
             text = "Не смогли определить тему вашего вопроса. Попробуйте перефразировать вопрос"
-            env_var['expected'] = 'retry'
+            users[user_id]['expected'] = 'retry'
             print("Awaiting retry")
-            env_var['timer'].cancel()
-            print(env_var['timer_desc'] + "  отменён")
-            env_var['timer'] = Timer(30.0, remind,
-                                     [message, "Я все еще хочу вам помочь. Попробуйте перефразировать вопрос."])
-            env_var['timer'].start()
-            env_var['timer_desc'] = "Ожидание перефразирования в первый раз"
-            env_var['try_count'] = 1
+            users[user_id]['timer'].cancel()
+            print(users[user_id]['timer_desc'] + "  отменён")
+            users[user_id]['timer'] = Timer(30.0, remind,
+                                    [user_id, "Я все еще хочу вам помочь. Попробуйте перефразировать вопрос."])
+            users[user_id]['timer'].start()
+            users[user_id]['timer_desc'] = "Ожидание перефразирования в первый раз"
+            users[user_id]['try_count'] = 1
         elif expected == 'retry':
-            if env_var['try_count'] < 2:
+            if users[user_id]['try_count'] < 2:
                 text = "Не смогли определить тему вашего вопроса. Попробуйте ещё раз"
-                env_var['try_count'] += 1
-                env_var['timer'].cancel()
-                print(env_var['timer_desc'] + "  отменён")
-                env_var['timer'] = Timer(30.0, remind,
-                                         [message, "Я все еще хочу вам помочь. Попробуйте перефразировать вопрос ещё раз."])
-                env_var['timer'].start()
-                env_var['timer_desc'] = "Ожидание перефразирования в %d раз" % env_var['try_count']
-                print("Try no. %d" % env_var['try_count'])
+                users[user_id]['try_count'] += 1
+                users[user_id]['timer'].cancel()
+                print(users[user_id]['timer_desc'] + "  отменён")
+                users[user_id]['timer'] = Timer(30.0, remind,
+                                         [user_id, "Я все еще хочу вам помочь. Попробуйте перефразировать вопрос ещё раз."])
+                users[user_id]['timer'].start()
+                users[user_id]['timer_desc'] = "Ожидание перефразирования в %d раз" % users[user_id]['try_count']
+                print("Try no. %d" % users[user_id]['try_count'])
             else:
                 text = "Ок, я запутался, давайте начнём заново :)"
-                env_var['expected'] = 'query'
-                env_var['try_count'] = 0
-                env_var['timer'].cancel()
-                print(env_var['timer_desc'] + "  отменён")
+                users[user_id]['expected'] = 'query'
+                users[user_id]['try_count'] = 0
+                users[user_id]['timer'].cancel()
+                print(users[user_id]['timer_desc'] + "  отменён")
         else:
             text = "Problemes"
     return text
 
 
-def remind(message, text):  # TODO написать
-    bot.send_message(message.chat.id, text)
-    env_var['timer'] = Timer(180.0, forget)  # TODO поставить 180
-    env_var['timer'].start()
-    env_var['timer_desc'] = "Ждём чтобы забыть"
+def remind(user_id, text):  # TODO написать
+    bot.send_message(user_id, text)
+    users[user_id]['timer'] = Timer(180.0, forget, [user_id])  # TODO поставить 180
+    users[user_id]['timer'].start()
+    users[user_id]['timer_desc'] = "Ждём чтобы забыть"
 
 
-def forget():
-    env_var['timer'].cancel()
-    print(env_var['timer_desc'] + " отменён")
-    env_var['expected'] = 'query'
+def forget(user_id):
+    users[user_id]['timer'].cancel()
+    print(users[user_id]['timer_desc'] + " отменён")
+    users[user_id]['expected'] = 'query'
 
 
 @bot.message_handler(commands=['start'])
@@ -122,46 +124,58 @@ def greeting(message):
 
 @bot.message_handler(content_types=['text'])
 def respond(message):
-    global env_var
+    global env_var, users
+    user_id = message.chat.id
+    if user_id not in users.keys():
+        users[user_id] = {
+            'last_theme': None,
+            'get_response': None,
+            'expected': 'query',
+            'timer': None,
+            'timer_desc': '',
+            'try_count': 0,
+            'context': None
+        }
+
     markup = None
     text = "Problemes"
     ans_type = classify_answer(message.text.lower())
     if ans_type:
-        text = check_confirmation(ans_type, env_var['expected'], message)
-    elif env_var['expected'] == 'choice':
+        text = check_confirmation(ans_type, users[user_id]['expected'], message)
+    elif users[user_id]['expected'] == 'choice':
         if message.text.lower().split(".")[1] == "никакая из предложенных":
             text = "Не смогли определить тему вашего вопроса. Попробуйте ещё раз"
-            env_var['expected'] = 'query'
+            users[user_id]['expected'] = 'query'
         else:
             num = int(message.text.lower().split('.')[0])
             if 0 < num < 5:
                 text = "В ближайшее время на ваш запрос ответит оператор"
-                env_var['expected'] = 'query'
+                users[user_id]['expected'] = 'query'
             else:
                 text = "Номер темы указан неправильно. Уточните, какая тема вас интересует."
-        env_var['timer'].cancel()
-        print(env_var['timer_desc'] + ' отменён')
+        users[user_id]['timer'].cancel()
+        print(users[user_id]['timer_desc'] + ' отменён')
     else:
-        if env_var['expected'] == 'retry':
-            env_var['context'] = env_var['context'] + ". " + message.text
-        elif env_var['expected'] == 'query' or env_var['expected'] == 'choice':
-            env_var['context'] = message.text
+        if users[user_id]['expected'] == 'retry':
+            users[user_id]['context'] = users[user_id]['context'] + ". " + message.text
+        elif users[user_id]['expected'] == 'query' or users[user_id]['expected'] == 'choice':
+            users[user_id]['context'] = message.text
         else:
             print("Expectations failed. Abort now!")
-        print(env_var['context'])
-        response = model.get_response(env_var['context'])
+        print(users[user_id]['context'])
+        response = model.get_response(users[user_id]['context'])
         markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
         if len(response) == 1:
             text = "Вас интересует тема \"%s\". Да?" % response[0][1]
-            env_var['last_theme'] = response[0][1]
-            if env_var['expected'] == 'query':
-                env_var['expected'] = 'confirmation'
-            if env_var['timer']:
-                env_var['timer'].cancel()
-                print(env_var['timer_desc'] + " отменён")
-            env_var['timer'] = Timer(30.0, remind, [message, "Мне нужен ваш ответ. Напишите \"Да\" или \"Нет\"."])
-            env_var['timer'].start()
-            env_var['timer_desc'] = "Напомнить про конфёрм"
+            users[user_id]['last_theme'] = response[0][1]
+            if users[user_id]['expected'] == 'query':
+                users[user_id]['expected'] = 'confirmation'
+            if users[user_id]['timer']:
+                users[user_id]['timer'].cancel()
+                print(users[user_id]['timer_desc'] + " отменён")
+            users[user_id]['timer'] = Timer(30.0, remind, [user_id, "Мне нужен ваш ответ. Напишите \"Да\" или \"Нет\"."])
+            users[user_id]['timer'].start()
+            users[user_id]['timer_desc'] = "Напомнить про конфёрм"
             markup.add('Да', 'Нет')
         elif len(response) < 5:
             text = "Пожалуйста, уточните, какая из тем вас интересует:"
@@ -171,14 +185,14 @@ def respond(message):
                 markup.add("%d. %s" % (i, theme[1]))
             i += 1
             markup.add("%d.Никакая из предложенных" % i)
-            env_var['expected'] = 'choice'
+            users[user_id]['expected'] = 'choice'
             # TODO ДОЛЛАРЫ
-            if env_var['timer']:
-                env_var['timer'].cancel()
-                print(env_var['timer_desc'] + " отменён")
-            env_var['timer'] = Timer(30.0, remind, [message, "Мне нужен ваш ответ. Выберите одну из предложенных тем."])
-            env_var['timer'].start()
-            env_var['desc'] = "Напомнить выбор"
+            if users[user_id]['timer']:
+                users[user_id]['timer'].cancel()
+                print(users[user_id]['timer_desc'] + " отменён")
+            users[user_id]['timer'] = Timer(30.0, remind, [user_id, "Мне нужен ваш ответ. Выберите одну из предложенных тем."])
+            users[user_id]['timer'].start()
+            users[user_id]['desc'] = "Напомнить выбор"
         else:
             text = "Произошла ошибка определения, обратитесь к Рыбкину."
 
